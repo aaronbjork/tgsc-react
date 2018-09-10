a5be99e9738d4d702314991a7bcdcde763f297b8
Aaron Bjork <aaronbjork@hotmail.com>
2018-09-10 11:09:35 -0700

    Default page updates.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to tgsc-react!</h1>
         </header>
         <p className="App-intro">
-          A react based scorecard. To get started, edit <code>src/App.js</code> and save to reload.
+          A react based scorecard. Try it out!
         </p>
       </div>
     );
